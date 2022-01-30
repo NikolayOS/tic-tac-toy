00a40ded53229c92eed9c97592407efe834a8b5e
Mykola Osadchii <nikolayosadchii842@gmail.com>
2022-01-30 12:31:22 +0200

    fixed by comment
    value names was fixed
    document formating was fixed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,43 +1,47 @@
 const area = document.getElementById("area");
 let move = 0;
-let res = "";
-area.addEventListener("click", e => {
-    if (e.target.className = "box"){
-        move % 2 === 0 ? e.target.innerHTML = "X" : e.target.innerHTML = "O";
-        move++;
-        
-    }
-check();
-})
+let winnerIs = "";
+
+area.addEventListener("click", (e) => {
+  if ((e.target.className = "box")) {
+    move % 2 === 0 ? (e.target.innerHTML = "X") : (e.target.innerHTML = "O");
+    move++;
+  }
+  check();
+});
+
 const check = () => {
-const arrayValues = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-];
-const boxValue = document.getElementsByClassName("box");
-for (i = 0; i < arrayValues.length; i++){
+  const winStateValues = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const boxValue = document.getElementsByClassName("box");
+  for (i = 0; i < winStateValues.length; i++) {
     if (
-        boxValue[arrayValues[i][0]].innerHTML == "X" && boxValue[arrayValues[i][1]].innerHTML == "X" && boxValue[arrayValues[i][2]].innerHTML == "X"
-    )
-    {
-    res = "Player X";
+      boxValue[winStateValues[i][0]].innerHTML == "X" &&
+      boxValue[winStateValues[i][1]].innerHTML == "X" &&
+      boxValue[winStateValues[i][2]].innerHTML == "X"
+    ) {
+      winnerIs = "Player X";
+    } else if (
+      boxValue[winStateValues[i][0]].innerHTML == "O" &&
+      boxValue[winStateValues[i][1]].innerHTML == "O" &&
+      boxValue[winStateValues[i][2]].innerHTML == "O"
+    ) {
+      winnerIs = "Player O";
     }
-    else if(
-        boxValue[arrayValues[i][0]].innerHTML == "O" && boxValue[arrayValues[i][1]].innerHTML == "O" && boxValue[arrayValues[i][2]].innerHTML == "O"
-    )
-    {
-    res = "Player O";
-    }
-}
-console.log(res)
-}
+  }
+  console.log(winnerIs);
+};
+
 let restart = document.getElementById("restart");
-restart.addEventListener("click",() => {
-    boxValue = "";
-})
+
+restart.addEventListener("click", () => {
+  boxValue = "";
+});

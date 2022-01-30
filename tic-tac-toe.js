79355508114a528085bee5815f5f8331b7e0e658
Mykola Osadchii <nikolayosadchii842@gmail.com>
2022-01-30 12:02:50 +0200

    game win logic was added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,4 +7,37 @@ area.addEventListener("click", e => {
         move++;
         
     }
+check();
+})
+const check = () => {
+const arrayValues = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+const boxValue = document.getElementsByClassName("box");
+for (i = 0; i < arrayValues.length; i++){
+    if (
+        boxValue[arrayValues[i][0]].innerHTML == "X" && boxValue[arrayValues[i][1]].innerHTML == "X" && boxValue[arrayValues[i][2]].innerHTML == "X"
+    )
+    {
+    res = "Player X";
+    }
+    else if(
+        boxValue[arrayValues[i][0]].innerHTML == "O" && boxValue[arrayValues[i][1]].innerHTML == "O" && boxValue[arrayValues[i][2]].innerHTML == "O"
+    )
+    {
+    res = "Player O";
+    }
+}
+console.log(res)
+}
+let restart = document.getElementById("restart");
+restart.addEventListener("click",() => {
+    boxValue = "";
 })
